e5eb01ea82b9ae7e264e0d10e1ab725ee40af09f
TryCatchB <pozdrakovzena@gmail.com>
2024-09-30 14:41:14 +0300

    Multipage-site

--- JS/Main.js
+++ JS/Main.js
@@ -1,36 +1,46 @@
+// Selectors
 const tabItem = document.querySelectorAll(".tabs__btn-item");
 const tabContent = document.querySelectorAll(".tabs__content-item");
-
-tabItem.forEach((element) => {
-  element.addEventListener("click", open);
-});
-
-function open(event) {
-  const tabTarget = event.target;
-  const button = tabTarget.dataset.button;
-
-  removeClass(tabItem, "tabs__btn-item_active");
-
-  tabTarget.classList.add("tabs__btn-item_active");
-
-  removeClass(tabContent, "tabs__content-item_active");
-
-  document
-    .querySelector(`#${button}`)
-    .classList.add("tabs__content-item_active");
-}
-
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu__list");
 
+function toggleClass({ items, className, addClass }) {
+  items.forEach((item) => {
+    item.classList[addClass ? "add" : "remove"](className);
+  });
+}
+
+function creatingData(items, className, addClass) {
+  return {
+    items,
+    className,
+    addClass,
+  };
+}
+
+// Event delegation for tabs
+document.querySelector(".tabs")?.addEventListener("click", (event) => {
+  const tabTarget = event.target.closest(".tabs__btn-item");
+  if (tabTarget) {
+    const button = tabTarget.dataset.button;
+
+    const tabsBtnData = creatingData(tabItem, "tabs__btn-item_active", false);
+    toggleClass(tabsBtnData);
+    tabTarget.classList.add("tabs__btn-item_active");
+
+    const tabsContentData = creatingData(
+      tabContent,
+      "tabs__content-item_active",
+      false
+    );
+    toggleClass(tabsContentData);
+    document
+      .querySelector(`#${button}`)
+      ?.classList.add("tabs__content-item_active");
+  }
+});
+
+// Menu button functionality
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("menu__list_active");
 });
-
-function removeClass(listItems, className) {
-  listItems.forEach((item) => {
-    if (item.classList.contains(className)) {
-      item.classList.remove(className);
-    }
-  });
-}
